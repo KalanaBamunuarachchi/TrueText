866cc117d65c4b07564381af9ce3500322b5407a
KalanaBamunuarachchi <kalana.bamunuarachchi@outlook.com>
2024-11-30 15:25:16 +0530

    style setup

---
+++ TrueText/Views/ScanPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TrueText;
+
+public partial class ScanPage : Window
+{
+    public ScanPage()
+    {
+        InitializeComponent();
+    }
+}
