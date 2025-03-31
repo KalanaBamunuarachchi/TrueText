c95ca103c8a50cfcb740eb13602784aedfe6851b
KalanaBamunuarachchi <kalana.bamunuarachchi@outlook.com>
2025-03-31 16:35:43 +0530

    style changes

---
+++ TrueText/Converters/StatusClassConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+
+namespace TrueText.Converters
+{
+    public class StatusClassConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = value?.ToString()?.ToLowerInvariant();
+
+            return status switch
+            {
+                "connected" => "Green Circle",
+                "disconnected" => "Red Circle",
+                _ => "Grey Circle"
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
